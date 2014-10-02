3264dc4ee85939b3be423da17afd56cbc692f9e7
MarkJKelly <mark.kelly.ie@gmail.com>
2014-10-02 16:37:44 +1000

    Phone auth

--- server.js
+++ server.js
@@ -1,127 +1,160 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var app = express();
+var randomstring = require("randomstring");
+var async = require("async");
+var request = require("request");
 
+var app = express();
 app.set('view engine', 'ejs');
 app.use(express.static("bower_components"));
 app.use(express.static("public"));
 app.use(express.bodyParser());
 
-var async = require("async"), request = require("request"), email = "mark.kelly@docusign.com", password = "password8", integratorKey = "DOCU-0d0ffcb8-b8a4-4fb2-a42b-8a7e29f561fc", templateId = "21B46E9C-A3C0-44E1-8B29-4FC23109864F", templateRoleName = "Signer", baseUrl = "https://demo.docusign.net/restapi/v2/accounts/378107";
+var email = "mark.kelly@docusign.com";
+var password = "password8";
+var integratorKey = "DOCU-0d0ffcb8-b8a4-4fb2-a42b-8a7e29f561fc";
+var templateId = "21B46E9C-A3C0-44E1-8B29-4FC23109864F";
+var templateRoleName = "Signer";
+var baseUrl = "https://demo.docusign.net/restapi/v2/accounts/378107";
 
 app.get('/', function(req, res) {
 
 	res.render('index');
 });
 
-app.post('/', function(req, res) {
+app
+		.post(
+				'/',
+				function(req, res) {
 
-	var signingUrl;
+					var signingUrl;
 
-	console.dir(req.body);
+					var clientUserId = randomstring.generate(20);
+					console.dir("clientUserId: [" + clientUserId + "]");
+					console.dir(req.body);
 
-	var fullname = req.body.fullname;
-	var signerEmail = req.body.signeremail;
-	var mobilePhone = req.body.mobilephone;
-	var requireIdLookup = false;
+					var fullname = req.body.fullname;
+					var signerEmail = req.body.signeremail;
+					var mobilePhone = req.body.mobilephone;
+					var requireIdLookup = false;
 
-	if (mobilePhone !== "") {
-		requireIdLookup = true;
-	}
-
-	async.series([
-
-	// ////////////////////////////////////////////////////////////////////
-	// Step 1 - Send envelope with one Embedded
-	// recipient (using clientUserId property)
-	// ////////////////////////////////////////////////////////////////////
-	function(next) {
-		var url = baseUrl + "/envelopes";
-		var body = JSON.stringify({
-			"emailSubject" : "DocuSign API call - Embedded Sending Example",
-			"emailBlurb" : "Please sign...thanks!",
-			"compositeTemplates" : [ {
-				"serverTemplates" : [ {
-					"sequence" : 1,
-					"templateId" : templateId
-
-				} ],
-				"inlineTemplates" : [ {
-					"sequence" : 2,
-					"recipients" : {
-						"signers" : [ {
-							"email" : signerEmail,
-							"name" : fullname,
-							"recipientId" : "1",
-							"clientUserId" : "1001",
-							"roleName" : templateRoleName,
-							"requireIdLookup" : requireIdLookup,
-							"idCheckConfigurationName" : "SMS Auth $",
-							"smsAuthentication" : {
-								"senderProvidedNumbers" : [ mobilePhone ]
-							}
-						} ]
+					if (mobilePhone !== "") {
+						requireIdLookup = true;
+						console.dir("requireIdLookup: [" + requireIdLookup
+								+ "]");
 					}
-				} ]
-			} ],
-			"status" : "sent"
-		});
 
-		// set request url, method, body, and
-		// headers
-		var options = initializeRequest(url, "POST", body, email, password);
+					async
+							.series([
 
-		// send the request...
-		console.dir(options);
-		request(options, function(err, res, body) {
-			if (!parseResponseBody(err, res, body)) {
-				return;
-			}
-			// parse the envelopeId
-			// value from the response
-			envelopeId = JSON.parse(body).envelopeId;
-			next(null); // call next
-			// function
-		});
-	},
+									// ////////////////////////////////////////////////////////////////////
+									// Step 1 - Send envelope with one Embedded
+									// recipient (using clientUserId property)
+									// ////////////////////////////////////////////////////////////////////
+									function(next) {
+										var url = baseUrl + "/envelopes";
+										var body = JSON
+												.stringify({
+													"emailSubject" : "Embedded Signing",
+													"emailBlurb" : "Please sign...thanks!",
+													"compositeTemplates" : [ {
+														"serverTemplates" : [ {
+															"sequence" : 1,
+															"templateId" : templateId
 
-	// ////////////////////////////////////////////////////////////////////
-	// Step 2 - Get the Embedded Signing View
-	// (aka the recipient view)
-	// ////////////////////////////////////////////////////////////////////
-	function(next) {
-		var url = baseUrl + "/envelopes/" + envelopeId + "/views/recipient";
-		var method = "POST";
-		var body = JSON.stringify({
-			"returnUrl" : "http://www.docusign.com/devcenter",
-			"authenticationMethod" : "email",
-			"email" : signerEmail,
-			"userName" : fullname,
-			"clientUserId" : "1001",
-		});
+														} ],
+														"inlineTemplates" : [ {
+															"sequence" : 2,
+															"recipients" : {
+																"signers" : [ {
+																	"email" : signerEmail,
+																	"name" : fullname,
+																	"recipientId" : "1",
+																	"clientUserId" : clientUserId,
+																	"roleName" : templateRoleName,
+																	"requireIdLookup" : requireIdLookup,
+																	"idCheckConfigurationName" : "Phone Auth $",
+																	"phoneAuthentication" : {
+																		"recipMayProvideNumber" : true,
+																		"validateRecipProvidedNumber" : true,
+																		"recordVoicePrint" : false,
+																		"senderProvidedNumbers" : [ mobilePhone ]
+																	}
+																} ]
+															}
+														} ]
+													} ],
+													"status" : "sent"
+												});
 
-		// set request url, method, body, and
-		// headers
-		var options = initializeRequest(url, "POST", body, email, password);
+										// set request url, method, body, and
+										// headers
+										var options = initializeRequest(url,
+												"POST", body, email, password);
 
-		// send the request...
-		request(options, function(err, res, body) {
-			if (!parseResponseBody(err, res, body))
-				return;
-			else
-				signingUrl = JSON.parse(body).url;
-			console.log(signingUrl);
-			next(null);
-		});
-	},
+										// send the request...
+										console.dir(options);
+										request(
+												options,
+												function(err, res, body) {
+													if (!parseResponseBody(err,
+															res, body)) {
+														return;
+													}
+													// parse the envelopeId
+													// value from the response
+													envelopeId = JSON
+															.parse(body).envelopeId;
+													next(null); // call next
+													// function
+												});
+									},
 
-	// ////////////////////////////////////////////////////////////////////
-	// Step 3 - Redirect to Signing View
-	// ////////////////////////////////////////////////////////////////////
-	function(err) {
-		res.redirect(signingUrl);
-	} ]);
-});
+									// ////////////////////////////////////////////////////////////////////
+									// Step 2 - Get the Embedded Signing View
+									// (aka the recipient view)
+									// ////////////////////////////////////////////////////////////////////
+									function(next) {
+										var url = baseUrl + "/envelopes/"
+												+ envelopeId
+												+ "/views/recipient";
+										var method = "POST";
+										var body = JSON
+												.stringify({
+													"returnUrl" : "http://www.docusign.com/devcenter",
+													"authenticationMethod" : "email",
+													"email" : signerEmail,
+													"userName" : fullname,
+													"clientUserId" : clientUserId,
+												});
+
+										// set request url, method, body, and
+										// headers
+										var options = initializeRequest(url,
+												"POST", body, email, password);
+
+										// send the request...
+										request(
+												options,
+												function(err, res, body) {
+													if (!parseResponseBody(err,
+															res, body))
+														return;
+													else
+														signingUrl = JSON
+																.parse(body).url;
+													console.log(signingUrl);
+													next(null);
+												});
+									},
+
+									// ////////////////////////////////////////////////////////////////////
+									// Step 3 - Redirect to Signing View
+									// ////////////////////////////////////////////////////////////////////
+									function(err) {
+										res.redirect(signingUrl);
+									} ]);
+				});
 
 // ***********************************************************************************************
 // --- HELPER FUNCTIONS ---
@@ -163,4 +196,5 @@ function parseResponseBody(err, res, body) {
 }
 
 var port = process.env.PORT || 3000;
+console.log("Port:" + port);
 app.listen(port);
