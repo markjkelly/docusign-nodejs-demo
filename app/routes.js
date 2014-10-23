18b483f71b675652f6aaa2b6bfec69f471b07f09
MarkJKelly <mark.kelly.ie@gmail.com>
2014-10-23 13:03:42 +1100

    Added / redirect

--- app/routes.js
+++ app/routes.js
@@ -8,6 +8,11 @@ var config = require('./../config.json');
 
 module.exports = function(app) {
 
+	app.get('/', function(req, res) {
+
+		res.render('index');
+	});
+	
 	app.get('/demo', function(req, res) {
 
 		res.render('index');
