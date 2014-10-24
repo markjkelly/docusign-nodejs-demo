16a4c46378458d2497fc2c0b96ef84a1109dfa99
MarkJKelly <mark.kelly.ie@gmail.com>
2014-10-24 16:30:12 +1100

    Upgraded to Express 4 Addded Port Environment Variable for OpenShift

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
 app.set('view engine', 'ejs');
 app.use(express.static("bower_components"));
 app.use(express.static("public"));
-app.use(express.bodyParser());
+app.use(bodyParser.urlencoded({
+	extended : true
+}));
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
-//load the routes
+// load the routes
 require('./app/routes')(app);
 
 app.listen(port);
